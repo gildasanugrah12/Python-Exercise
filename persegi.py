395cee43e0cf6c1c6f482fc8319d1a721d5f080b
= <=>
2024-10-01 11:53:12 +0700

    create aplikasi persegi

---
+++ persegi.py
@@ -0,0 +1,11 @@
+print('-'*20)
+print('PERSEGI')
+print('-'*20)
+
+sisi = int(input('Masukan nilai sisi :'))
+
+luas = sisi * sisi 
+keliling = 4 * sisi 
+
+print('luas persegi :',luas,'cm2')
+print('keliling persegi :',keliling,'cm2') 
